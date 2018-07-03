73639ac0034241876f5cb03c7e58a032a5b4d6c8
Tzuson <40787713+Tzuson@users.noreply.github.com>
2018-07-03 17:00:19 +0200

    Add files via upload

---
+++ home.R
@@ -0,0 +1,10 @@
+source("initialization")
+source("functions/functions")
+source("functions/measures")
+# source("data/nsnet")
+
+
+measures(g,global_efficiency)
+
+
+
---
+++ initialization.R
@@ -0,0 +1,14 @@
+# Used packages
+suppressPackageStartupMessages({
+  library(igraph)
+  library(tidyr)
+  library(dplyr)
+  library(tidyverse)
+  library(magrittr)
+  library(stringdist)
+  library(rvest)
+  library(xml2)
+})
+
+
+
